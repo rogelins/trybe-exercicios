a07608d95f8af549094b44a7836221ea70a0f4e1
rogelins <rogelins@gmail.com>
2022-04-07 08:51:12 -0300

    primeiros exercicios de JavaScript

--- Fundamentals/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios_4.1_part6.js
+++ Fundamentals/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios_4.1_part6.js
@@ -3,10 +3,9 @@ chessPiece = "bishop";
 bishop = "diagonals";
 rooks = "horizontally or vertically";
 
-if(chessPiece == "Bishop") {
+if(stri == "Bishop") {
     console.log(bishop);
 }
 else {
     console.log("Invalid")
 }
-
